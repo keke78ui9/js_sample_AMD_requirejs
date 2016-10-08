5c4d2ea0db42c31c051156e7faa21dc74eebe77c
keke78ui9 <keke78ui9@gmail.com>
2016-10-07 22:18:36 -0700

    javascript with AMD requirejs

---
+++ README.md
@@ -0,0 +1,12 @@
+# js_sample_AMD_requirejs
+simple json beautify build by javascript with module pattern
+
+using requirejs, AMD pattern
+
+```
+npm install -g live-server
+```
+
+```
+live-server
+```
---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+;define(["jsonBeautify", "jsCache"], function(_json, _cache, undefined){
+    'strict'
+    var 
+        jsonTextArea = document.getElementById("jsonTextArea"),
+        actionButton = document.getElementById("actionButton"),
+        minifyButton = document.getElementById("minifyButton"),
+        displayPanel = document.getElementById("displayPanel")
+        ;
+
+    function prettyFormat() {
+        if (!jsonTextArea.value) {
+            return;
+        }
+
+        var result = _json.beautifyJson(jsonTextArea.value);
+        if (result.flg) {
+            jsonTextArea.value = result.value;
+        }
+        else {
+            panel("Error => " + result.error);
+        }
+    };
+
+    function minify() {
+        if (!jsonTextArea.value) {
+            return;
+        }
+        var result = _json.minifyJson(jsonTextArea.value);
+        if (result.flg) {
+            jsonTextArea.value = result.value;
+             _cache.store(result.value);
+        }
+        else {
+            panel("Error => " + result.error);
+        }
+    }
+
+    function panel(message) {
+        if (!message) {
+            return;
+        }
+        displayPanel.innerHTML += "Message: " + message + "</br>";
+    }
+
+    function init() {
+       // register events
+        actionButton.onclick = prettyFormat;
+        minifyButton.onclick = minify;
+    }
+
+    init();
+});
